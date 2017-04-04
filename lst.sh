d2c8812ccb20fab67aa7521a0c096aeb2d706b46
Marcel Jepma <m.jepma@jispro.nl>
2017-04-04 09:09:27 +0200

    Environment based URL

--- lst.sh
+++ lst.sh
@@ -9,7 +9,7 @@ while [  $COUNTER -lt 100000 ]; do
   [ $((COUNTER%2)) -eq 0 ] && SETTINGS="-c 100 -r 5000"
 
   # RUN LST
-  docker run --net=host --rm weaveworksdemos/load-test -d 5 -h demo.5bd1269b11fc4c7c128e.kpnappfactory.nl $SETTINGS
+  docker run --net=host --rm weaveworksdemos/load-test -d 5 -h $LSTHOST $SETTINGS
 
   # Update counter
   let COUNTER=COUNTER+1
